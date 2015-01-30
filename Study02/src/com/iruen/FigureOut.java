83414f83acc26a305f64dbe9655d4ded8344742b
iruentech2 <iruentech2@Iruentech-PC>
2015-01-30 10:06:52 +0900

    tt

---
+++ Study02/src/com/iruen/FigureOut.java
@@ -0,0 +1,7 @@
+package com.iruen;
+
+public class FigureOut {
+	public static void output(){
+		System.out.println("ют╥б");
+	}
+}
---
+++ Study02/src/com/iruen/FigureOverr.java
@@ -0,0 +1,7 @@
+package com.iruen;
+
+public class FigureOverr extends FigureOut{
+	public static void output(){
+		System.out.println("도형이 입력되었습니다.");
+	}
+}
