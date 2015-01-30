f801b2d804b902d74fc218d8c8f305368a20c70b
iruentech2 <iruentech2@Iruentech-PC>
2015-01-30 10:04:35 +0900

    수정사항

--- Study02/src/com/iruen/FigureController.java
+++ Study02/src/com/iruen/FigureController.java
@@ -7,15 +7,27 @@ import java.util.List;
 public class FigureController {
 	List<String> figureList = new ArrayList<String>();
 	
+	//도형을 입력할지 조회 선택
+	public void figureChose(){
+		MenuBar.menuFigure();
+		MenuBar.menuFigKey();
+	}
+	//도형입력을 선택시 입력
 	public void figureInput(){
-		for(int i=0;i<3;i++){
+		if(figureList.size()<3){ 
 			System.out.println("도형을 입력하세요 ");
 			String figure = MenuBar.scan.next();
 			figureList.add(figure);
+			FigureOverr.output();
+			MenuBar.menuFigure();
+			MenuBar.menuFigKey();
+		}else{
+			System.out.println("제한된 도형의 갯수를 다 입력하셨습니다.");
+			MenuBar.menuSee();
+			MenuBar.menuKey();
 		}
-		MenuBar.menuSee();
-		MenuBar.menuKey();
-	}
+	}    
+	//첫메뉴에서 조회
 	public void searchFigure(){
 		if(figureList.size()==0){
 			System.out.println("입력된 도형이 없습니다.");
@@ -30,5 +42,21 @@ public class FigureController {
 		}
 		MenuBar.menuSee();
 		MenuBar.menuKey();
+	}
+	//안쪽메뉴에서 조회
+	public void searchFigure1(){
+		if(figureList.size()==0){
+			System.out.println("입력된 도형이 없습니다.");
+		}else{
+		System.out.println("정해진 갯수의 도형을 다 입력하셨습니다.");
+		System.out.println("======입력한 도형=====");
+		for(int i=0;i<figureList.size();i++){
+			String result=figureList.get(i);
+			System.out.println(result+" ");
+		}
+		System.out.println("==================");
+		}
+		MenuBar.menuFigure();
+		MenuBar.menuFigKey();
 	}
 }
--- Study02/src/com/iruen/Study0127.java
+++ Study02/src/com/iruen/Study0127.java
@@ -1,33 +1,5 @@
 package com.iruen;
 
-import java.util.Scanner;
-
-class MenuBar {
-	public static Scanner scan = new Scanner(System.in);
-	static FigureController fgcon = new FigureController();
-	static String input;
-
-	public static void menuSee() {
-		System.out.println("==================");
-		System.out.println("도형을 입력하세요(1.입력 2.조회  3.종료)");
-		System.out.println("입력 : ");
-		input = MenuBar.scan.next();
-	}
-
-	public static void menuKey() {
-		switch (input) {
-		case "1":
-			fgcon.figureInput();
-			break;
-		case "2":
-			fgcon.searchFigure();
-			break;
-		case "3":
-			System.out.println("프로그램을 종료합니다.");
-			break;
-		}
-	}
-}
 
 public class Study0127 {
 
